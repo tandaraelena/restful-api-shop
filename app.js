1f74302d3505bf9a724e3d1c7d1153f34c7ef939
tandaraelena <tandaraelena@yahoo.com>
2019-11-26 11:27:58 +0000

    error handling in case of any unknown route

--- app.js
+++ app.js
@@ -7,8 +7,23 @@ const ordersRoutes = require('./api/routes/orders')
 
 app.use(morgan('dev'))
 
-// setup a midlleware and here every request it's finaled to
+// routes which will handle the requests 
 app.use('/products', productsRoutes);
 app.use('/orders', ordersRoutes)
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message
+    }
+  })
+})
 
 module.exports = app;
