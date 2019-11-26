74605b8686b034b209b159a2e31c8ae28fd49fd2
tandaraelena <tandaraelena@yahoo.com>
2019-11-26 17:51:39 +0000

    patch(update) the products

--- api/routes/products.js
+++ api/routes/products.js
@@ -32,7 +32,7 @@ router.post('/', (req, res, next) => {
     .save()
     .then(result => {
       console.log(result);;
-      result.status(200).json({
+      res.status(200).json({
         message: "Handling POST requests to /products",
         createdProduct: result
       });
@@ -69,9 +69,22 @@ router.get('/:productId', (req, res, next) => {
 });
 
 router.patch('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Updated product'
-  })
+  const id = req.params.productId;
+  const updateOps = {};
+
+  for(const ops of req.body){
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then(result => {
+      console.log(result);
+      res.status(200).json(result)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json({message: err})
+    })
 });
 
 router.delete('/:productId', (req, res, next) => {
