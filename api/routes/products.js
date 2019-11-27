fafdf827a335f3ed6a37128af5ff4a6f0c32a1c2
tandaraelena <tandaraelena@yahoo.com>
2019-11-27 22:07:46 +0000

    make the product's field to be requiered and the response to look nicer

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,11 +6,26 @@ const Product = require('../models/product')
 
 // .get() it's the method that handles the GET requests. First param it says which subroute to handle and the second one it's a handler
 router.get('/', (req, res, next) => {
+  // .select accepts the exactly fields that you want to fetch
   Product.find()
+    .select("name price _id")
     .exec()
     .then(results => {
-      console.log(results);
-      res.status(200).json(results);
+      const response = {
+        count: results.length,
+        products: results.map(r => {
+          return {
+            name: r.name,
+            price: r.price,
+            _id: r._id,
+            request: {
+              type: "GET",
+              url: "http://localhost:3000/products/" + r._id
+            }
+          }
+        })
+      }
+      res.status(200).json(response);
     })
     .catch(err => {
       console.log(err);
