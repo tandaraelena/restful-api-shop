dc24cd0e5109d23313f33ec4688b212c649e1308
tandaraelena <tandaraelena@yahoo.com>
2019-12-03 21:19:20 +0000

    handle the get and post requests to the orders

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,23 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose')
+
+const Order = require('../models/order')
 
 // .get() it's the method that handles the GET requests. 
 // First param it says which subroute to handle and the second one it's a handler
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling GET requests to /orders'
-  });
+  Order.find()
+    .select("_id quantity")
+    .exec()
+    .then(docs => {
+      res.status(200).json({
+        count: docs.length,
+        orders: docs.map(r => ({
+          orderId: r._id,
+          orderQuantity: r.quantity,
+        }))
+      })
+    })
+    .catch(err => res.status(500).json({error: err}))
 });
 
 router.post('/', (req, res, next) => {
-  const order = {
-    productId: req.body.productId,
-    quantity: req.body.quantity
-  };
-  res.status(201).json({
-    message: "Handling POST requests to /orders",
-    order
+  const order = new Order({
+    _id: new mongoose.Types.ObjectId(),
+    quantity: req.body.quantity,
+    product: req.body.productId
   });
+
+  order.save()
+    .then(result => {
+      console.log(result)
+      res.status(201).json(result)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      })
+    })
 });
 
 router.get('/:orderId', (req, res, next) => {
